49bf14117c096982d287332337a4aeae0d95a40e
tamarablezinky <tamarablezinky05@gmail.com>
2024-09-23 17:11:00 +0700

    Add Greeter

---
+++ src/main/kotlin/id/ac/polbeng/depandi/test_kelas/Greeter.kt
@@ -0,0 +1,7 @@
+package id.ac.polbeng.depandi.test_kelas
+
+class Greeter{
+    fun greet(){
+        println ("Hello object world!")
+    }
+}
---
+++ src/main/kotlin/id/ac/polbeng/depandi/test_kelas/GreeterTest.kt
@@ -0,0 +1,6 @@
+package id.ac.polbeng.depandi.test_kelas
+
+fun main(){
+    val greeter = Greeter()
+    greeter.greet()
+}

570ff1ba5248d5884bbb1d26df956f5070aba149
tamarablezinky <tamarablezinky05@gmail.com>
2024-09-23 17:20:17 +0700

    Add Greeter.kt

--- src/main/kotlin/id/ac/polbeng/depandi/test_kelas/Greeter.kt
+++ src/main/kotlin/id/ac/polbeng/depandi/test_kelas/Greeter.kt
@@ -1,7 +1,14 @@
 package id.ac.polbeng.depandi.test_kelas
 
-class Greeter{
-    fun greet(){
-        println ("Hello object world!")
+class Greeter {
+    var text: String = ""
+    fun greet() {
+        println("Hello object world!")
+    }
+    fun greet(name: String) {
+        println("$text $name")
+    }
+    fun with_ret_val(name: String): String {
+        return "$text $name"
     }
 }
--- src/main/kotlin/id/ac/polbeng/depandi/test_kelas/GreeterTest.kt
+++ src/main/kotlin/id/ac/polbeng/depandi/test_kelas/GreeterTest.kt
@@ -1,6 +1,11 @@
 package id.ac.polbeng.depandi.test_kelas
 
-fun main(){
+fun main() {
     val greeter = Greeter()
     greeter.greet()
+    greeter.text = "Hi"
+    greeter.greet("Anton")
+    greeter.greet("Budi")
+    greeter.text = "Hello programmer"
+    println(greeter.with_ret_val("Dono"))
 }

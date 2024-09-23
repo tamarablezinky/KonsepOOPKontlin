fce92e5aaa15a245d4415d1e66944c57d5ee0b12
tamarablezinky <tamarablezinky05@gmail.com>
2024-09-23 19:59:51 +0700

    Add PersonD.kt

--- src/main/kotlin/id/ac/polbeng/depandi/test_kelas/PersonD.kt
+++ src/main/kotlin/id/ac/polbeng/depandi/test_kelas/PersonD.kt
@@ -1,2 +1,19 @@
 package id.ac.polbeng.depandi.test_kelas
 
+
+class PersonD {
+    var firstName: String
+    var lastName: String
+    var age: Int
+    constructor(_firstName: String, _lastName: String, _age: Int){
+        firstName = _firstName
+        lastName = _lastName
+        age = _age
+    }
+}
+
+fun main(){
+    val budi = PersonD("Budi", "Gunawan", 21)
+    println("Name : ${budi.firstName} ${budi.lastName}")
+    println("Age : ${budi.age}")
+} 

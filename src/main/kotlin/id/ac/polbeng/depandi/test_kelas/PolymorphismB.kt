e22eea6671898925d4fbfe8ed76f8d303ff17c50
tamarablezinky <tamarablezinky05@gmail.com>
2024-09-25 21:03:33 +0700

    Add PolymorphismB.kt

---
+++ src/main/kotlin/id/ac/polbeng/depandi/test_kelas/PolymorphismB.kt
@@ -0,0 +1,19 @@
+package id.ac.polbeng.depandi.test_kelas
+
+open class MyBase {
+   
+    open fun think () {
+        println("Hey!! i am thinking ")
+    }
+}
+class MyDerived: MyBase() {
+    override fun think() {
+        println("I Am from Child")
+    }
+}
+fun main() {
+    val myBase = MyBase()
+    myBase.think()
+    var myDerived = MyDerived()
+    myDerived.think()
+}

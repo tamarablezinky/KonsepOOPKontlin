3a16229fbc8d876a45ca2c95607d0cf40c87b60f
tamarablezinky <tamarablezinky05@gmail.com>
2024-09-25 22:16:14 +0700

    Add NestedInnerClassB.kt

---
+++ src/main/kotlin/id/ac/polbeng/depandi/test_kelas/NestedInnerClassB.kt
@@ -0,0 +1,14 @@
+package id.ac.polbeng.depandi.test_kelas
+
+fun main() {
+    // creating an instance of the interface
+    var programmer: Human = object : Human {
+        override fun think() { // overriding the think method
+            print("I am an example of Anonymous Inner Class ")
+        }
+    }
+    programmer.think()
+}
+interface Human {
+    fun think()
+}
